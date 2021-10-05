364db5e5647c343d473465373996656b0701a2d0
Andrew Marques <acmarques.ds@gmail.com>
2021-10-05 11:12:16 -0300

    added test set loading

--- loader.py
+++ loader.py
@@ -1,26 +1,34 @@
 #%%
 import utils
 from preprocessing import preprocessor
-import pathlib
 import joblib
 from matplotlib import pyplot as plt
 
 #%%
-data_dir = './GTSRB/Training/Images' 
-data_dir = pathlib.Path(data_dir)
-
-image_count = len(list(data_dir.glob('*/*.ppm'))) #number of images in dataset
-print(image_count)
-
-trainImages, trainLabels = utils.readTrafficSigns('./GTSRB/Training/Images', (32, 32))
-print(len(trainLabels), len(trainImages))
-plt.imshow(trainImages[42])
+X_train, y_train = utils.readTrafficSigns('./GTSRB','training', (32, 32))
+print(len(y_train), len(X_train))
+plt.imshow(X_train[42])
 plt.show()
 
-joblib.dump(trainImages, 'images.joblib')
-joblib.dump(trainLabels, 'labels.joblib')
+#%%
+joblib.dump(X_train, 'train_images.joblib')
+joblib.dump(y_train, 'train_labels.joblib')
 
 #%%
-processedImages = preprocessor(trainImages)
-joblib.dump(processedImages, 'procImages.joblib')
+X_test, y_test = utils.readTrafficSigns('./GTSRB','test', (32, 32))
+print(len(y_test), len(X_test))
+plt.imshow(X_test[42])
+plt.show()
 
+#%%
+joblib.dump(X_test, 'test_images.joblib')
+joblib.dump(y_test, 'test_labels.joblib')
+
+#%%
+processedImages = preprocessor(X_train)
+joblib.dump(processedImages, 'procTrainImages.joblib')
+
+# %%
+processedTestImages = preprocessor(X_test)
+joblib.dump(processedTestImages, 'procTestImages.joblib')
+# %%
